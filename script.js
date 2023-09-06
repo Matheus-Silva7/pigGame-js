02a5182670b18da7240915f30e6c69992b7fd80d
Matheus Silva <mate.almeidasilva@gmail.com>
2023-09-06 14:46:33 -0300

    finalizando game

--- script.js
+++ script.js
@@ -1,109 +1,60 @@
 const imgDice = document.querySelector('.dice').src = "/img/start-game.png"
-let score = document.querySelector('.score')
-let player0 = document.querySelector('.player--active')
-let player1 = document.querySelector('.player--active')
+let player0 = document.querySelector('.player--0');
+let player1 = document.querySelector('.player--1');
+let score0 = document.getElementById('score--0')
+let score1 = document.getElementById('score--1')
 let currentScore0 = document.getElementById('current--0')
-let currentScore1 = document.getElementById('current--0')
+let currentScore1 = document.getElementById('current--1')
 let newCurrentScore = 0;
-let player = document.querySelector('.player')
-let checkActive = true
+let newScore0 = 0;
+let newScore1 = 0;
 
 
-if (player0.classList.contains('player--active')){
+const btnRoll = document.querySelector('.btn--roll').addEventListener('click', () => {
 
-    const btnRoll = document.querySelector('.btn--roll').addEventListener('click', () => {
-        
-        let randomNumber = Math.trunc(Math.random() * 6) + 1
-        console.log(randomNumber)
-        
-        if (randomNumber === 2) {
-            
-            document.querySelector('.dice').src = "/img/dice-2.png"
-            newCurrentScore += randomNumber;
-            currentScore0.textContent = newCurrentScore
-            
-        } else if (randomNumber === 3) {
-            
-            document.querySelector('.dice').src = "/img/dice-3.png"
-            newCurrentScore += randomNumber;
-            currentScore0.textContent = newCurrentScore
-            
-        } else if (randomNumber === 4) {
-            document.querySelector('.dice').src = "/img/dice-4.png"
-            newCurrentScore += randomNumber;
-            currentScore0.textContent = newCurrentScore
-        } else if (randomNumber === 5) {
-            
-        document.querySelector('.dice').src = "/img/dice-5.png"
-        newCurrentScore += randomNumber;
-        currentScore0.textContent = newCurrentScore
-        
-    } else if (randomNumber === 6) {
-        
-        document.querySelector('.dice').src = "/img/dice-6.png"
-        newCurrentScore += randomNumber;
-        currentScore0.textContent = newCurrentScore
-        
-    } else if (randomNumber === 1) {
-        
+    let randomNumber = Math.trunc(Math.random() * 6) + 1
+    console.log(randomNumber)
+
+    if (randomNumber === 1) {
         document.querySelector('.dice').src = "/img/dice-1.png"
-        currentScore0.textContent = 0
-        newCurrentScore = 0
-        currentScore0 = 0
-        player0.classList.remove('player--active')
-        player1.classList.add('player--active')
+        newCurrentScore = 0;
+        currentScore0.textContent = '0';
+        currentScore1.textContent = '0';
+        player0.classList.toggle('player--active');
+        player1.classList.toggle('player--active');
+    } else {
+        document.querySelector('.dice').src = `/img/dice-${randomNumber}.png`
+        newCurrentScore += randomNumber;
+        if (player0.classList.contains('player--active')) {
+            currentScore0.textContent = newCurrentScore
+        } else {
+            currentScore1.textContent = newCurrentScore
+        }
     }
 })
-} else if(player1.classList.contains('player--active')){
-    const btnRoll = document.querySelector('.btn--roll').addEventListener('click', () => {
-        
-        let randomNumber = Math.trunc(Math.random() * 6) + 1
-        console.log(randomNumber)
-        
-        if (randomNumber === 2) {
-            
-            document.querySelector('.dice').src = "/img/dice-2.png"
-            newCurrentScore += randomNumber;
-            currentScore1.textContent = newCurrentScore
-            
-        } else if (randomNumber === 3) {
-            
-            document.querySelector('.dice').src = "/img/dice-3.png"
-            newCurrentScore += randomNumber;
-            currentScore1.textContent = newCurrentScore
-            
-        } else if (randomNumber === 4) {
-            document.querySelector('.dice').src = "/img/dice-4.png"
-            newCurrentScore += randomNumber;
-            currentScore1.textContent = newCurrentScore
-        } else if (randomNumber === 5) {
-            
-        document.querySelector('.dice').src = "/img/dice-5.png"
-        newCurrentScore += randomNumber;
-        currentScore1.textContent = newCurrentScore
-        
-    } else if (randomNumber === 6) {
-        
-        document.querySelector('.dice').src = "/img/dice-6.png"
-        newCurrentScore += randomNumber;
-        currentScore1.textContent = newCurrentScore
-        
-    } else if (randomNumber === 1) {
-        
-        document.querySelector('.dice').src = "/img/dice-1.png"
-        currentScore1.textContent = 0
-        newCurrentScore = 0
-        currentScore1 = 0
-        player1.classList.remove('player--active')
-        player0.classList.add('player--active')
-    }
-})
-}
 
 
 const bntHold = document.querySelector('.btn--hold').addEventListener('click', () => {
-    let newScore = 0;
-    newScore += newCurrentScore
-    score.textContent = newScore
-    score += newScore
+    if (player0.classList.contains('player--active')) {
+        newScore0 += newCurrentScore
+        score0.textContent = newScore0
+    } else {
+        newScore1 += newCurrentScore
+        score1.textContent = newScore1
+    }
+    player0.classList.toggle('player--active');
+    player1.classList.toggle('player--active');
+    newCurrentScore = 0;
+    currentScore0.textContent = '0';
+    currentScore1.textContent = '0';
+    if (newScore0 >= 100) {
+        alert("Jogador 1 Vence!")
+        newScore0 = 0
+        score0.textContent = newScore0
+    } else if (newScore1 >= 100) {
+        alert("Jogador 2 venceu")
+        newScore1 = 0
+        score1.textContent = newScore1
+    }
 })
+
